d3cf1977993665aa868b94bf09fdc739c7907834
Ramòn Janssen <ramonj@live.nl>
2015-12-16 10:59:16 +0100

    init

---
+++ src/w14filelockingparttwo/W14FileLockingPartTwo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w14filelockingparttwo;
+
+/**
+ *
+ * @author Ramòn Janssen
+ */
+public class W14FileLockingPartTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
